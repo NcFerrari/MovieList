aef08eaa49f24e1fbfccf1fa77c62ed7f1c8b373
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-05-26 08:57:12 +0200

    setting scrollpane vvalue for current category

--- MovieList/.idea/sonarlint/securityhotspotstore/index.pb
+++ MovieList/.idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,9 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 <
 IMPORT2.json,c\b\cb34f7070a039f8a7cd30b2e0ac285f170739ea6
+a
+1src/main/java/lp/serviceimpl/FileServiceImpl.java,9\c\9c5926a4d96f0b5277350274a556c31493d3b28d
+M
+src/main/java/lp/Manager.java,3\9\39315997c19bf78f312829fc7762910ecb70e9f1
+;
+IMPORT.json,b\1\b19aa95263b1d2b7a5bdf7667b85990dac3e271e
--- MovieList/src/main/java/lp/frontend/StartApp.java
+++ MovieList/src/main/java/lp/frontend/StartApp.java
@@ -20,6 +20,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lp.business.dto.Episode;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.util.ArrayList;
@@ -48,7 +50,7 @@ public class StartApp extends Application {
     private FlowPane menuPane;
     private ScrollPane choiceScrollPane;
     private VBox choicePane;
-    private String currentEpisodeTitle;
+    private CategoryInfo currentCategorySelected;
 
     public void start(Stage stage) {
         mainPane = new BorderPane();
@@ -86,24 +88,30 @@ public class StartApp extends Application {
         if (getImportedEpisode().getEpisodes().isEmpty()) {
             return;
         }
-        getImportedEpisode().getEpisodes().forEach(episode -> {
-            Button menuButton = new Button(episode.getTitle());
+        getImportedEpisode().getEpisodes().forEach(category -> {
+            Button menuButton = new Button(category.getTitle());
             addStyle(menuButton, StyleClasses.MENU_BUTTON);
             buttonList.add(menuButton);
             menuPane.setMargin(menuButton, new Insets(10));
             menuPane.getChildren().add(menuButton);
 
             menuButton.setOnMouseClicked(evt -> {
+                if (currentCategorySelected != null) {
+                    currentCategorySelected.setScrollPanePosition(choiceScrollPane.getVvalue());
+                }
                 setSelectedButton(menuButton);
-                if (!episodeMap.containsKey(episode.getTitle())) {
-                    List<CheckBox> newCheckBoxList = new ArrayList<>();
-                    episode.getEpisodes().stream().forEach(subEpisode -> {
-                        CheckBox checkBox = new CheckBox(subEpisode.getTitle());
-                        addStyle(checkBox, StyleClasses.CHECKBOX);
-                        Tooltip checkBoxTooltip = new Tooltip(subEpisode.getTitle());
+                if (!episodeMap.containsKey(category.getTitle())) {
+                    List<Item> newItemList = new ArrayList<>();
+                    category.getEpisodes().stream().forEach(episode -> {
+                        Item item = new Item(new CheckBox(episode.getTitle()));
+                        addStyle(item.getCheckBox(), StyleClasses.CHECKBOX);
+                        Tooltip checkBoxTooltip = new Tooltip(episode.getTitle());
                         checkBoxTooltip.setFont(new Font("Arial", 16));
-                        checkBox.setTooltip(checkBoxTooltip);
-                        if (!subEpisode.getEpisodes().isEmpty()) {
+                        item.getCheckBox().setTooltip(checkBoxTooltip);
+                        if (!episode.getEpisodes().isEmpty()) {
+                            episode.getEpisodes().stream().forEach(subEpisode -> {
+                                item.getCheckBoxes().add(new CheckBox(subEpisode.getTitle()));
+                            });
                             ToggleButton toggleButton = new ToggleButton(">");
                             toggleButton.setOnAction(action -> {
                                 if (toggleButton.isSelected()) {
@@ -112,29 +120,29 @@ public class StartApp extends Application {
                                     toggleButton.setText(">");
                                 }
                             });
-                            checkBox.setGraphic(toggleButton);
+                            item.getCheckBox().setGraphic(toggleButton);
                         }
-                        newCheckBoxList.add(checkBox);
+                        newItemList.add(item);
                     });
-                    episodeMap.put(episode.getTitle(), new CategoryInfo(0.0f, newCheckBoxList));
+                    episodeMap.put(category.getTitle(), new CategoryInfo(0.0, newItemList));
                 }
+                currentCategorySelected = episodeMap.get(menuButton.getText());
                 resize();
-                fillChoicePane(episode.getTitle());
+                fillChoicePane(category.getTitle());
             });
         });
     }
 
     private void fillChoicePane(String title) {
-        choicePane.getChildren().clear();
-        episodeMap.get(currentEpisodeTitle).setScrollPanePosition(choiceScrollPane.getVvalue());
-        if (episodeMap.containsKey(title)) {
-            currentEpisodeTitle = title;
-        }
-        episodeMap.get(currentEpisodeTitle).getList().stream().forEach(checkBox -> {
-            checkBox.setPrefWidth(mainPane.getWidth() / 2 - 20);
-            choicePane.getChildren().add(checkBox);
+        choicePane = new VBox();
+        choicePane.setStyle("-fx-background-color: #7e6969");
+        choicePane.setMinWidth(mainPane.getWidth() / 2);
+        choiceScrollPane.setContent(choicePane);
+        episodeMap.get(title).getItems().stream().forEach(item -> {
+            item.getCheckBox().setPrefWidth(mainPane.getWidth() / 2 - 20);
+            choicePane.getChildren().add(item.getCheckBox());
         });
-
+        choiceScrollPane.setVvalue(episodeMap.get(title).getScrollPanePosition());
     }
 
     private void resize() {
@@ -173,6 +181,16 @@ public class StartApp extends Application {
     @AllArgsConstructor
     private class CategoryInfo {
         private double scrollPanePosition;
-        private List<CheckBox> list;
+        private List<Item> items;
+    }
+
+    @Data
+    private class Item {
+        private CheckBox checkBox;
+        private List<CheckBox> checkBoxes = new ArrayList<>();
+
+        public Item(CheckBox checkBox) {
+            this.checkBox = checkBox;
+        }
     }
 }
